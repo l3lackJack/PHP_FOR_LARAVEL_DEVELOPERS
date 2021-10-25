2da9bbc14d51cadc2d554291a8460b4dc23aab89
l3lackJack <gabrielionuturs@iesebre.com>
2021-10-25 19:41:06 +0200

    Afegida funcio connectDB

--- app/index.php
+++ app/index.php
@@ -1,25 +1,14 @@
 <?php
-
+require 'config.php';
 require 'app/helpers.php';
 require 'app/Task.php';
-require 'config.php';
 
 //$user = 'debian-sys-maint';
 //$pass = 'y1LrMYathEWbKfc3';
 //$dsn = 'mysql:host=localhost;dbname=phplaraveldevs';
-
-
-try {
-    $dbh = new PDO($config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
-        $config['database']['user'],
-        $config['database']['password']);
-} catch (\Exception $e) {
-    echo 'Error de connexio a la base de dades';
-}
+$dbh = ConnectDB($config);-
 $statement = $dbh->prepare('SELECT * FROM tasks;');
-
 $statement->execute();
 $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-
 $greeting = greet();
 
--- config.php
+++ config.php
@@ -9,3 +9,12 @@ $config=[
         'name'=> 'phplaraveldevs',
     ]
 ];
+function ConnectDB($config){
+try {
+     return new PDO($config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
+} catch (\Exception $e) {
+    echo 'Error de connexio a la base de dades';
+}
+}
