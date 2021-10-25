d24967050e2b935cd6a6dca630d08fc42efe5ff7
l3lackJack <gabrielionuturs@iesebre.com>
2021-10-25 19:20:17 +0200

    Separar la configuracio(config.php) del codi

--- app/index.php
+++ app/index.php
@@ -8,26 +8,18 @@ require 'config.php';
 //$pass = 'y1LrMYathEWbKfc3';
 //$dsn = 'mysql:host=localhost;dbname=phplaraveldevs';
 
-$user = $config['database']['user'];
-$pass = $config['database']['password'];
-$type = $config['database']['databasetype'];
-$host = $config['database']['host'];
-$name = $config['database']['host'];
-$dsn = "$type:host=$host;dbname=$name";
-
 
 try {
-    $dbh = new PDO('m', $user, $pass);
-
-}catch (\Exception $e){
+    $dbh = new PDO($config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
+} catch (\Exception $e) {
     echo 'Error de connexio a la base de dades';
 }
-$statement= $dbh->prepare('SELECT * FROM tasks;');
+$statement = $dbh->prepare('SELECT * FROM tasks;');
 
 $statement->execute();
-$tasks = $statement->fetchAll(PDD::FECTH_CLASS , 'Task');
-
-var_dump($tasks);
+$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
 
 $greeting = greet();
 
