2eb2a1de1de6a9c020adb7bbc2b8ced0d88cf425
l3lackJack <gabrielionuturs@iesebre.com>
2021-10-26 20:09:46 +0200

    Video 104 minut 12:40

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 return [
     'database' => [
-        'user' => 'debian-sys-maint',
-        'password' => 'y1LrMYathEWbKfc3',
-        'databasetype' => 'mysql',
-        'host' => 'localhost',
-        'name' => 'phplaraveldevs',
+        'user' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'databasetype' => $_ENV['DB_CONNECTION'],
+        'host' => $_ENV['DB_HOST'],
+        'name' => $_ENV['DB_DATABASE'],
     ]
 ];
+
 
 
--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+
+use Dotenv\Dotenv;
 use framework\App;
 use framework\Database\Connection;
 use framework\Database\Database;
@@ -9,3 +11,5 @@ App::bind('database', new Database(
     Connection::make(App::get('config')['database'])
 ));
 
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
